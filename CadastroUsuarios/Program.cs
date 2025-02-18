2c8b50465fd52528f1d51fabd9cbfefa53c60c86
Alisson <alisson.csilva16@senacsp.edu.br>
2025-02-18 20:00:52 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroUsuarios/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CadastroUsuarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número de 1 a 7:");
+            int numero = int.Parse(Console.ReadLine());
+
+            string diaDaSemana = numero switch
+            {
+                1 => "Domingo",
+                2 => "Segunda-feira",
+                3 => "Terça-feira",
+                4 => "Quarta-feira",
+                5 => "Quinta-feira",
+                6 => "Sexta-feira",
+                7 => "Sábado",
+                _ => "Número inválido. Por favor, digite um número de 1 a 7."
+            };
+
+            Console.WriteLine(diaDaSemana);
+        }
+    }
+}
---
+++ StatusAluno/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace StatusAluno
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o Nome do Aluno: ");
+            string nome = Console.ReadLine()!;
+            Console.Write("\n");
+            Console.Write("Informe a Nota 1: ");
+            //float nota1 = Convert.ToSingle(Console.ReadLine()!);
+            float nota1 = float.Parse(Console.ReadLine()!);
+            Console.Write("Informe a Nota 2: ");
+            float nota2 = float.Parse(Console.ReadLine()!);
+
+            float soma = nota1 + nota2;
+            float media = soma / 2;
+            float status = media
+
+            Console.WriteLine($"A média de {nome} é {media}");
+
+            if (media >= 6.0)
+            {
+                Console.WriteLine("Aluno aprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Aluno reprovado.");
+            }
+
+        }
+    }
+}
