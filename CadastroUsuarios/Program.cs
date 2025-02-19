0bfc25e7114fd6c3e1b870cca0bf2e762449d833
Alisson <alisson.csilva16@senacsp.edu.br>
2025-02-18 21:33:15 -0300

    feat: exercicios corrigidos

--- CadastroUsuarios/Program.cs
+++ CadastroUsuarios/Program.cs
@@ -4,22 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite um número de 1 a 7:");
-            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe um número de 1 a 7 para exibir o Dia da Semana");
+            string opcao = Console.ReadLine()!;
 
-            string diaDaSemana = numero switch
+            switch (opcao)
             {
-                1 => "Domingo",
-                2 => "Segunda-feira",
-                3 => "Terça-feira",
-                4 => "Quarta-feira",
-                5 => "Quinta-feira",
-                6 => "Sexta-feira",
-                7 => "Sábado",
-                _ => "Número inválido. Por favor, digite um número de 1 a 7."
-            };
+                case "1":
+                    Console.WriteLine("Domingo");
+                    break;
+                case "2":
+                    Console.WriteLine("Segunda-feira");
+                    break;
+                case "3":
+                    Console.WriteLine("Terça-feira");
+                    break;
+                case "4":
+                    Console.WriteLine("Quarta-feira");
+                    break;
+                case "5":
+                    Console.WriteLine("Quinta-feira");
+                    break;
+                case "6":
+                    Console.WriteLine("Sexta-feira");
+                    break;
+                case "7":
+                    Console.WriteLine("Sábado");
+                    break;
+                default:
+                    Console.WriteLine("Dia Inválido");
+                    break;
+            }
 
-            Console.WriteLine(diaDaSemana);
+
         }
     }
 }
--- StatusAluno/Program.cs
+++ StatusAluno/Program.cs
@@ -4,29 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Informe o Nome do Aluno: ");
-            string nome = Console.ReadLine()!;
+            /*
+             * Faça um algoritmo para a leitura de duas notas parciais 
+             * de um aluno.
+             * O algoritmo deve calcular a média alcançada por aluno 
+             * e apresentar:
+            
+                A mensagem "Aprovado", se a média alcançada for maior ou igual a sete;
+                A mensagem "Reprovado", se a média for menor do que cinco;
+                A mensagem “Recuperação", se a média estiver entre cinco e sete.
+            */
+            double nota1 = 0;
+            double nota2 = 0;
+            string mensagem = "";
+            double media = 0;
+
+            Console.Write("Informe a 1ª nota: ");
+            nota1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("\n");
-            Console.Write("Informe a Nota 1: ");
-            //float nota1 = Convert.ToSingle(Console.ReadLine()!);
-            float nota1 = float.Parse(Console.ReadLine()!);
-            Console.Write("Informe a Nota 2: ");
-            float nota2 = float.Parse(Console.ReadLine()!);
+            Console.Write("Informe a 2ª nota: ");
+            nota2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\n");
+            media = (nota1 + nota2) / 2;
 
-            float soma = nota1 + nota2;
-            float media = soma / 2;
-            float status = media
-
-            Console.WriteLine($"A média de {nome} é {media}");
-
-            if (media >= 6.0)
+            if (media >= 7)
             {
-                Console.WriteLine("Aluno aprovado!");
+                mensagem = "Aprovado";
+            }
+            else if (media >= 5 && media < 7)
+            {
+                mensagem = "Recuperação";
             }
             else
             {
-                Console.WriteLine("Aluno reprovado.");
+                mensagem = "Reprovado";
             }
+            Console.WriteLine($"O Status do Aluno de Média: {media} é {mensagem}");
 
         }
     }
